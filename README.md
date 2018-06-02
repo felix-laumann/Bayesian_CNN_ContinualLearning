9483325505ad60b8ccbb4ff8e0bc2c4f6b923c56
felix-laumann <laumannfelix@gmail.com>
2018-06-02 13:25:29 +0200

    work on extract

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Continual learning with a Bayesian CNN
 
 ## Instructions
-1. Run `main_continual.py` with `pretrained = False`
-2. Run `main_continual.py` with `pretrained = True`, and `task = 2`
-3. Run `main_continual.py` with `pretrained = True`, and `task = 3, 4, 5,` and so on
+`main_continual.py` is the main file. Run it each time with the subsequent hyperparameters configurations:
+
+1. `pretrained = False` will give you `weights_1.pkl` (everything else as it is by default)
+2. `pretrained = True`, and `task = 2` will give you `weights_2.pkl` to evaluate forward transfer
+3. `is_training = False`, `pretrained = True`, `task = 2` to evaluate backward transfer, i.e. ability to overcome catastrophic forgetting
+4. `pretrained = True`, `task = 3`  will give you `weights_3.pkl`
+5. `is_training = False`, `pretrained = True`, `task = 3`
+6. (and so on for as many tasks as you want)
 
 
 [Bayesian CNN repository](https://github.com/felix-laumann/Bayesian_CNN)
--- extraction/extract_continual.py
+++ extraction/extract_continual.py
@@ -14,15 +14,14 @@ tasks = 2  # tasks continually learnt
 
 def load_data(tasks):
     os.chdir("/home/felix/Dropbox/publications/Bayesian_CNN_continual/results/")
-    files = [open("diagnostics_{}.txt".format(task)).read() for task in range(tasks)]
 
-    for file in files:
-        with open(file, 'r') as f:
-            acc = re.findall(r"'acc':\s+tensor\((.*?)\)", f.read())
+    for task in range(1, tasks):
+        with open("diagnostics_{}.txt".format(task), 'r') as file:
+            acc = re.findall(r"'acc':\s+tensor\((.*?)\)", file.read())
         print(acc)
 
-        train[file] = acc[1::2]
-        valid[file] = acc[0::2]
+        train = acc[1::2]
+        valid = acc[0::2]
 
         return np.array(train).astype(np.float32), np.array(valid).astype(np.float32)
 
@@ -32,15 +31,16 @@ colors = sns.color_palette(n_colors=tasks)
 
 train, valid = load_data(tasks=tasks)
 
-print(valid)
+#print(valid)
+#print(valid.shape[1])
 
 plt.plot(valid, "--", label=r"Validation, prior: $U(a, b)$", color='maroon')
-#plt.plot(i, valid, "--", label=r"Validation, prior: $q(w | \theta_A)$", color='navy')
+plt.plot(valid, "--", label=r"Validation, prior: $q(w | \theta_A)$", color='navy')
 
 
 plt.xlabel("Epochs")
 plt.ylabel("Accuracy")
-x_ticks = range(len(valid.shape[1]))
+x_ticks = range(len(valid))
 plt.xticks(x_ticks[9::10], map(lambda x: x+1, x_ticks[9::10]))
 
 f.suptitle("Evaluating continual learning")
